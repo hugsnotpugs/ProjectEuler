d0bf97ca4494da1484720c685c9481db8d0d5771
Chuan He <che721@gmail.com>
2014-07-27 18:08:48 -0400

    Update Euler4.py

--- Euler4.py
+++ Euler4.py
@@ -1,9 +1,11 @@
 ''' Project Euler #4:
     Find the largest palindrome that is the product of two 3 digit numbers '''
 
+
+### The factor range is inclusive
 def max_palindrome(min_factor, max_factor):
     palindrome = 0
-    for i in range(min_factor, max_factor+1):
+    for i in range(min_factor, max_factor+1): 
         for j in range(min_factor, max_factor+1):
             product = i*j
             length = len(str(product))
